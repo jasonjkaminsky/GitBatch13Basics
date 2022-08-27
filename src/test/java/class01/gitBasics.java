ca6b1bde0c5df2ffcb0d72f8132b848f34f0d0e0
jasonjkaminsky <jasonjkaminsky@gmail.com>
2022-08-27 09:38:47 -0500

    three lines of additional code

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -3,6 +3,8 @@ package class01;
 
 public class gitBasics {
     public static void main(String[] args) {
-        System.out.println("today is a beautiful day");
+        System.out.println("today is not a beautiful day");
+        System.out.println("today is not a beautiful day");
+        System.out.println("today is not a beautiful day");
     }
 }
